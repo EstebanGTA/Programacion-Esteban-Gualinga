26ce4a2f140ae54242474cb41f81ff729ad22b6d
EstebanGTA <eat.gualinga@yavirac.edu.ec>
2022-12-06 23:03:52 -0800

    Guardar archivo

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hola mundo")
+class persona:
+            nombre = str
+            edad = int
+            
+            def _init_(self, nombre, edad):
+                self.nombre = nombre
+                self.edad = edad
+            def saluda (self, otra_persona):
+                return f'Hola {otra_persona.nombre}'
