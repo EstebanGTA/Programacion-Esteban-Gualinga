68a01bd9941762a839a12ea65da98e96ab5dad87
EstebanGTA <eat.gualinga@yavirac.edu.ec>
2022-12-09 11:01:22 -0800

    enviar ultimo ejercicio

---
+++ ejercicio3.py
@@ -0,0 +1,15 @@
+#Mostrar series de Fibonacci hasta 10 términos.
+
+a=0
+b=1
+c=0
+while True:
+    n=9
+    break
+print("1",end=" ")
+for i in range(0,n):
+    c=a+b
+    print(f"{c}",end=" ")
+    a=b
+    b=c
+print("")
