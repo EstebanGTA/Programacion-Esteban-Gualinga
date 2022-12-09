3941f64e0c2d1b85daa2265d0f0e0869fecfba2a
EstebanGTA <eat.gualinga@yavirac.edu.ec>
2022-12-09 00:00:52 -0800

    subir los 2 ejercicios

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+#6.Cree un bucle que sume los números del 100 al 200
+
+e = 100
+suma = 0
+while e <= 200:
+    suma = suma + e
+    e += 1
+    
+print("La suma de los números de 100 al 200 es: ", suma)
